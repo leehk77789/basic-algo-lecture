6b217686c646354cb4dc3c86b6fee98e58f121f4
110110 <127813398+june4969@users.noreply.github.com>
2023-10-07 01:04:24 +0900

    키로거/상은

---
+++ 0x04/SE_5397.py
@@ -0,0 +1,23 @@
+T = int(input())
+
+for _ in range(T):
+    left_stack = []
+    right_stack = []
+    cmd = input()
+
+
+    for i in cmd:
+        if i == '<':
+            if left_stack:
+                right_stack.append(left_stack.pop())
+        elif i == '>':
+            if right_stack:
+                left_stack.append(right_stack.pop())
+        elif i == '-':
+            if left_stack: 
+                left_stack.pop() 
+        else: 
+            left_stack.append(i)
+    left_stack.extend(reversed(right_stack))
+    
+    print(''.join(left_stack))
